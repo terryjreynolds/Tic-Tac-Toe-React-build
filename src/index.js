a682b982382b47ee92ac9e7e937371fbb78a58e0
Terry Reynolds <terrancejreynolds@gmail.com>
2018-08-04 16:50:53 -0400

    made all the funcs into methods on the GameBoard comp and tidied up onClicks

--- src/index.js
+++ src/index.js
@@ -224,16 +224,28 @@ class GameBoard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: ["X","O","X",
-              "O","O","",
-              "O","", "O"],
+      board: ["","","",
+              "","","",
+              "","", ""],
               whosTurn: "player"
     };
    
     this.handleClick = this.handleClick.bind(this);
+    this.checkForWinOrDraw = this.checkForWinOrDraw.bind(this);
+    this.checkForDraw = this.checkForDraw.bind(this);
+    this.declareWinner = this.declareWinner.bind(this);
+    this.lightUpSquares = this.lightUpSquares.bind(this);
+    this.timingSquareLighting = this.timingSquareLighting.bind(this);
+    this.checkForClearBoard = this.checkForClearBoard.bind(this);
+    this.boardPopulation = this.boardPopulation.bind(this);
+    this.checkForAIWinningMove = this.checkForAIWinningMove.bind(this);
+    this.stateReferenceConverter = this.stateReferenceConverter.bind(this);
+    this.ai_MoveUpdate = this.ai_MoveUpdate.bind(this);
+    this.shouldAIBlock = this.shouldAIBlock.bind(this);
+    this.pickAGoodFirstAIMove = this.pickAGoodFirstAIMove.bind(this);
     
   }
-  
+   
   handleClick = (int, token) => {
     if(this.state.whosTurn === "player") {
     console.log('int', int, 'token', token);
@@ -249,108 +261,91 @@ class GameBoard extends React.Component {
     board: newArray,
     whosTurn: theOtherGuy
 },
-() => {tryItOut(this.state.board)}
+() => {this.checkForWinOrDraw(this.state.board, this.state)}
  );
  
-
-    }
-   
+}
+  }
+  
+//----------------------------------------AI Logic-----------------------------
+ai_MoveUpdate(int, token) {
+  const newArray = this.state.board.slice();   
     
-   console.log('board', this.state.board, 'whosTurn', this.state.whosTurn);
-   // checkForWinOrDraw(this.state.board, this.state);
-  }
-
-  render() {
-    return (
-      <div className="game-board">
-                <button 
-                className="square" id="sq0" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq0", 0, this.props.player_token)}}}>
-               {this.state.board[0]}
-                </button>
-                <button 
-                className="square" id="sq1" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq1", 1, this.props.player_token)}}}>
-                {this.state.board[1]}
-                </button>
-                <button 
-                className="square" id="sq2" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq1", 2, this.props.player_token)}}}>
-                {this.state.board[2]}
-                </button>
-                <button 
-                className="square" id="sq3" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq3", 3, this.props.player_token)}}}>
-                {this.state.board[3]}
-                </button>
-                <button 
-                className="square" id="sq4" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq4", 4, this.props.player_token)}}}>
-                {this.state.board[4]}
-                </button>
-                <button 
-                className="square" id="sq5" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq5", 5, this.props.player_token)}}}>
-                {this.state.board[5]}
-                </button>
-                <button 
-                className="square" id="sq6" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq6", 6, this.props.player_token)}}}>
-                {this.state.board[6]}
-                </button>
-                <button 
-                className="square" id="sq7" onClick={() => this.handleClick(7, this.props.player_token)}>
-                {this.state.board[7]}
-                </button>
-                <button 
-                className="square" id="sq8" onClick={(e) => {if(this.props.whosTurn === "player") {this.props.updateBoard("sq8", 8, this.props.player_token)}}}>
-                {this.state.board[8]}
-                </button>
-      </div>
-    );               
- }  
+ newArray.splice(int, 1, token);
+ console.log('newArray', newArray);
+ let theOtherGuy = this.state.whosTurn;
+    theOtherGuy === "player" ?  theOtherGuy = "AI" : theOtherGuy = "player";
+  this.setState(
+    {
+       board: newArray,
+       whosTurn: theOtherGuy
+   },
+   () => {this.checkForWinOrDraw(this.state.board, this.state)}
+    );
 }
-class ScoreBoard extends React.Component {
-  render() {
-    return (
-      <h2 className="scoreboard">Player:<span className="player-integer"> 0</span> Computer: <span className="computer-integer"> 0</span></h2>
+
+shouldAIBlock() {
+  console.log('im in shouldAIBlock');
+}
+
+pickAGoodFirstAIMove() {
+  console.log('Im in pickAGoodFirstAIMove');
+}
+
+checkForClearBoard(arr, state) {
      
-    );
-  }
+  console.log('called checkForClearBoard');
+  console.log('attheclearboardfunction', state)
+  //if the board is clear, call a function to choose a best first move; if not, call
+  //checkforaWinningMove
+  const boardIsEmpty = this.boardPopulation(state);
+  console.log('boardIsEmpty', boardIsEmpty);
+  boardIsEmpty === 9 ? this.pickAGoodFirstAIMove() : this.checkForAIWinningMove(arr, state); 
 }
-class ResetButton extends React.Component {
-  render() {
-    return (
-      <button className="reset-button" onClick={(e) => this.props.action()}>Reset Game</button>
-    );
-  }
+ boardPopulation(state) {
+  console.log('im in boardPopulation', state.board);
+  return (state.board.filter((c) => c === "")).length; 
 }
-class LevelSelector extends React.Component {
-  render() {
-    return (     
-      <div className="level-selector">Level 
-        <button className="sm-button" id="button1" onClick={(e) => {this.props.action("btn1"); this.props.buttonToggleOne()}}>1</button>or 
-        <button className="sm-button" id="button2" onClick={(e) =>{ this.props.action("btn2"); this.props.buttonToggleTwo()}}>2</button>?
-      </div>         
-    );
-  } 
-}
-class TokenSelector extends React.Component {
-  render() {
-    return (
-      <div className="token-selector">Level 
-        <button className="sm-button" id="_x" onClick={(e) => {this.props.action("btnX");  this.props.buttonToggleX("_x")}}>X</button>or 
-        <button className="sm-button" id="_o" onClick={(e) => {this.props.action("btnO"); this.props.buttonToggleO("_o")}}>O</button>?
-      </div>
-    );
-  }
-}
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">Tic Tac Toe in React designed and coded by T. Reynolds, 2018.</footer>
-    );
-  }
+stateReferenceConverter(a, b) {
+  console.log('inside machine', a, b);
+return a === 0 && b === 0 ? 0 : a === 0 && b === 1 ?  1 : a === 0 && b === 2 ?  2 : a === 1 && b === 0 ? 3 : a === 1 && b === 1 ? 4 : a === 1 && b === 2 ? 5 : a === 2 && b === 0 ? 6 : a === 2 && b === 1 ? 7 : a === 2 && b === 2 ? 8 : a === 3 && b === 0 ? 0 : a === 3 && b === 1 ? 3 : a === 3 && b === 2 ? 6 : a === 4 && b === 0 ? 1 : a === 4 && b === 1 ? 4 : a === 4 && b === 2 ? 7 : a === 5 && b === 0 ? 2 : a === 5 && b === 1 ? 5 : a === 5 && b === 2 ? 8 : a === 6 && b === 0 ? 0 : a === 6 && b === 1 ? 4 : a === 6 && b === 2 ? 8 : a === 7 && b === 0 ? 2 : a === 7 && b === 1 ? 4 : a === 7 && b === 2 ? 6 : "";
+    
+    }
+checkForAIWinningMove(arr, state) {
+  console.log('checkForAIWinningMove State:', state);
+  
+  const array = [[arr[0],arr[1],arr[2]], [arr[3],arr[4],arr[5]], [arr[6],arr[7],arr[8]], 
+  [arr[0],arr[3],arr[6]], [arr[1],arr[4],arr[7]], [arr[2],arr[5],arr[8]], 
+  [arr[0],arr[4],arr[8]], [arr[2],arr[4],arr[6]] ];
+  console.log('array',array);
+  //find an array among arrays that has two AI tokens and one empty string
+ const filtered =  (array.map((c,i) => (c.filter((d) => d !== this.props.player_token).length)).indexOf(3));
+ console.log('filtered', filtered);
+
+ //if filtered has an array with two AI tokens and one empty string, find the empty string so
+ //AI may make a move in that square for the win
+ 
+ if(filtered !== -1) {
+  const targetIndex = (array[filtered].map((c,i) => c === "")).indexOf(true);
+  console.log('targetIndex', targetIndex);
+ 
+  const element = this.stateReferenceConverter(filtered, targetIndex);
+  console.log('element', element);
+
+  let ai_token = "X";
+  this.props.player_token === "X" ? ai_token = "O" : ai_token = "X";
+ 
+ this.ai_MoveUpdate (element, ai_token);
+
+}else {
+   this.shouldAIBlock()
+  
+ }
+ 
 }
 
-//----------------JS Helper Functions-------------------------
-function tryItOut(state) {
-  console.log(state);
-}
-function checkForWinOrDraw(arr, state) {
+//----------------------------------------Game Logic---------------------------
+checkForWinOrDraw(arr, state) {
   console.log('stateincheckForWinOrDraw', state);
   //an array of all the possible winning rows
    const array = [[arr[0],arr[1],arr[2]], [arr[3],arr[4],arr[5]], [arr[6],arr[7],arr[8]], 
@@ -363,9 +358,9 @@ function checkForWinOrDraw(arr, state) {
   console.log('trueIndexO', trueIndexO);
   //this round of code golf just checks to see if any of the arrays within the array are either
   //all Xs or all Os-- if neither, then check for draw before deciding whos turn it is.
-  trueIndexX !== -1 ? declareWinner(array[trueIndexX], trueIndexX, state) : trueIndexO !== -1 ? declareWinner(array[trueIndexO], trueIndexO, state) : checkForDraw(arr, state); 
+  trueIndexX !== -1 ? this.declareWinner(array[trueIndexX], trueIndexX, state) : trueIndexO !== -1 ? this.declareWinner(array[trueIndexO], trueIndexO, state) : this.checkForDraw(arr, state); 
 }
- function checkForDraw(arr, state) {
+ checkForDraw(arr, state) {
    console.log('state in checkForDraw',state);
   //check for draw after checking for win
   if(arr.every((c) => c !== "")) {
@@ -375,11 +370,12 @@ function checkForWinOrDraw(arr, state) {
     const turn = state.whosTurn
     console.log('turn', turn);
      if(turn === "AI") {
-       checkForClearBoard(arr, state);
+       this.checkForClearBoard(arr, state);
+       //this is where the AI move will end if whosTurn is not AI, and the AI move starts with //checkForClearBoard
      }
   }
 }
-function declareWinner(winningRow, rowIndex, state) {
+declareWinner(winningRow, rowIndex, state) {
   console.log("index", rowIndex);
   console.log(winningRow);
   //check to see what player matches the letter in the winningRow and call and post to the screen
@@ -389,9 +385,9 @@ function declareWinner(winningRow, rowIndex, state) {
     console.log('Computer Wins');
   }
   //light up the winning squares
-  lightUpSquares(rowIndex);
+  this.lightUpSquares(rowIndex);
 } 
- function lightUpSquares(rowIndex) {
+ lightUpSquares(rowIndex) {
    let a;
    let b;
    let c;
@@ -442,72 +438,110 @@ function declareWinner(winningRow, rowIndex, state) {
         b = 1;
         c = 2;
   }
-    timingSquareLighting(a,b,c);  
+    this.timingSquareLighting(a,b,c);  
   }
 
-function timingSquareLighting(A,B,C) {
+ timingSquareLighting(A,B,C) {
   document.getElementById('sq'+ A).classList.toggle("square-lighted");
     setTimeout(() => {document.getElementById('sq'+ B).classList.toggle("square-lighted")}, 500);
     setTimeout(() => {document.getElementById('sq'+ C).classList.toggle("square-lighted");}, 1000); 
 }
-
-//---------------------------AI LOGIC---------------------------------
-
-function checkForClearBoard(arr, state){
-  console.log('called checkForClearBoard');
-  //if the board is clear, call a function to choose a best first move; if not, call
-  //checkforaWinningMove
-  const boardIsEmpty = boardPopulation(state);
-  console.log('boardIsEmpty', boardIsEmpty);
-  boardIsEmpty === 9 ? pickAGoodFirstAIMove() : checkForAIWinningMove(arr, state); 
-}
-function boardPopulation(state) {
-  console.log('im in boardPopulation', state.board);
-  return (state.board.filter((c) => c === "")).length; 
-}
-function checkForAIWinningMove(arr, state) {
-  const array = [[arr[0],arr[1],arr[2]], [arr[3],arr[4],arr[5]], [arr[6],arr[7],arr[8]], 
-  [arr[0],arr[3],arr[6]], [arr[1],arr[4],arr[7]], [arr[2],arr[5],arr[8]], 
-  [arr[0],arr[4],arr[8]], [arr[2],arr[4],arr[6]] ];
-  //find an array among arrays that has two AI tokens and one empty string
- const filtered =  (array.map((c,i) => (c.filter((d) => d !== state.player_token).length)).indexOf(3));
- console.log('filtered', filtered);
-
- //if filtered has an array with two AI tokens and one empty string, find the empty string so
- //AI may make a move in that square for the win
- 
- if(filtered !== -1) {
-  const targetIndex = (array[filtered].map((c,i) => c === "")).indexOf(true);
-  console.log('targetIndex', targetIndex);
- 
-  const element = stateReferenceConverter(filtered, targetIndex);
-  console.log('element', element);
-  function stateReferenceConverter(a, b) {
-    console.log('inside machine', a, b);
-  return a === 0 && b === 0 ? 0 : a === 0 && b === 1 ?  1 : a === 0 && b === 2 ?  2 : a === 1 && b === 0 ? 3 : a === 1 && b === 1 ? 4 : a === 1 && b === 2 ? 5 : "";
-      
-      }
-  let ai_token = "X";
-  state.player_token === "X" ? ai_token = "O" : ai_token = "X";
- window.AppComponent.updateBoard(element, ai_token);
   
-}else {
-   shouldAIBlock() 
-  
- }
 
- 
-  
+  render() {
+    return (
+      <div className="game-board">
+                <button 
+                className="square" id="sq0" onClick={() => this.handleClick(0, this.props.player_token)}>
+               {this.state.board[0]}
+                </button>
+                <button 
+                className="square" id="sq1" onClick={() => this.handleClick(1, this.props.player_token)}>
+                {this.state.board[1]}
+                </button>
+                <button 
+                className="square" id="sq2" onClick={() => this.handleClick(2, this.props.player_token)}>
+                {this.state.board[2]}
+                </button>
+                <button 
+                className="square" id="sq3" onClick={() => this.handleClick(3, this.props.player_token)}>
+                {this.state.board[3]}
+                </button>
+                <button 
+                className="square" id="sq4" onClick={() => this.handleClick(4, this.props.player_token)}>
+                {this.state.board[4]}
+                </button>
+                <button 
+                className="square" id="sq5" onClick={() => this.handleClick(5, this.props.player_token)}>
+                {this.state.board[5]}
+                </button>
+                <button 
+                className="square" id="sq6" onClick={() => this.handleClick(6, this.props.player_token)}>
+                {this.state.board[6]}
+                </button>
+                <button 
+                className="square" id="sq7" onClick={() => this.handleClick(7, this.props.player_token)}>
+                {this.state.board[7]}
+                </button>
+                <button 
+                className="square" id="sq8" onClick={() => this.handleClick(8, this.props.player_token)}>
+                {this.state.board[8]}
+                </button>
+      </div>
+    );               
+ }  
+}
+class ScoreBoard extends React.Component {
+  render() {
+    return (
+      <h2 className="scoreboard">Player:<span className="player-integer"> 0</span> Computer: <span className="computer-integer"> 0</span></h2>
+     
+    );
+  }
+}
+class ResetButton extends React.Component {
+  render() {
+    return (
+      <button className="reset-button" onClick={(e) => this.props.action()}>Reset Game</button>
+    );
+  }
+}
+class LevelSelector extends React.Component {
+  render() {
+    return (     
+      <div className="level-selector">Level 
+        <button className="sm-button" id="button1" onClick={(e) => {this.props.action("btn1"); this.props.buttonToggleOne()}}>1</button>or 
+        <button className="sm-button" id="button2" onClick={(e) =>{ this.props.action("btn2"); this.props.buttonToggleTwo()}}>2</button>?
+      </div>         
+    );
+  } 
+}
+class TokenSelector extends React.Component {
+  render() {
+    return (
+      <div className="token-selector">Level 
+        <button className="sm-button" id="_x" onClick={(e) => {this.props.action("btnX");  this.props.buttonToggleX("_x")}}>X</button>or 
+        <button className="sm-button" id="_o" onClick={(e) => {this.props.action("btnO"); this.props.buttonToggleO("_o")}}>O</button>?
+      </div>
+    );
+  }
+}
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">Tic Tac Toe in React designed and coded by T. Reynolds, 2018.</footer>
+    );
+  }
 }
 
 
-function shouldAIBlock() {
-  console.log('im in shouldAIBlock');
-}
 
-function pickAGoodFirstAIMove() {
-  console.log('Im in pickAGoodFirstAIMove');
-}
+
+
+
+
+
+
 
 
 
